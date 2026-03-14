f86edc9aead2a17485a88313bdcbd33dd1229751
Choi Yong Won <cywin0821@gmail.com>
2026-03-15 00:15:52 +0900

    fix: 운영및 로컬환경 구분 설정

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    const apiBaseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3030';
+
+    return {
+      beforeFiles: [
+        {
+          source: '/api/:path*',
+          destination: `${apiBaseUrl}/:path*`,
+        },
+      ],
+    };
+  },
 };
 
 module.exports = nextConfig;
